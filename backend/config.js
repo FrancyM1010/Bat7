5fe31ec0f836d2b6560c163b6571b3a03e0f1db3
Francy101001 <126831168+Francy101001@users.noreply.github.com>
2024-09-05 09:57:32 -0500

    changes backend

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  jwtSecret: 'your_jwt_secret',
-  mongoURI: 'mongodb+srv://<user>:<password>@cluster0.mongodb.net/bat7?retryWrites=true&w=majority'
+  jwtSecret: 'JuegoVocacional',
+  mongoURI: 'mongodb+srv://francyyuranimoreno1010:BaRDrGR7AHTIw7vv@cluster0.zt0wn.mongodb.net/bat7?retryWrites=true&w=majority'
 };
